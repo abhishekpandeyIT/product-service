318c494038c79acb85dd903059db205185a593de
abhishekpandeyIT <abhishekpandeyit@gmail.com>
2025-04-01 05:01:44 +0530

    Added files for prod deployment

--- server.js
+++ server.js
@@ -1,26 +1,21 @@
-require('dotenv').config();
 const express = require('express');
 const { CosmosClient } = require('@azure/cosmos');
-const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
+app.use(require('cors')());
 app.use(express.json());
 
-// Cosmos DB connection
+// Cosmos DB Connection
 const cosmosClient = new CosmosClient(process.env.COSMOS_DB_CONNECTION_STRING);
-const database = cosmosClient.database(process.env.COSMOS_DB_NAME);
-const container = database.container(process.env.COSMOS_DB_CONTAINER);
+const container = cosmosClient.database(process.env.COSMOS_DB_NAME).container(process.env.COSMOS_DB_CONTAINER);
 
-// Health check endpoint
-app.get('/', (req, res) => {
-  res.send('Product Service is running');
-});
+// Health Check
+app.get('/', (req, res) => res.send('Product Service is running'));
 
-// Get all products
+// API Routes
 app.get('/products', async (req, res) => {
   try {
     const { resources } = await container.items.query('SELECT * FROM c').fetchAll();
